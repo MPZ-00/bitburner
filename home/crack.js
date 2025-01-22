d404e4993f53a1f2583e72c058194243c5e747c6
MPZ-00 <martin.praetzlich@outlook.de>
2025-01-22 03:05:38 +0100

    Refactors target server cracking logic
    Enhances error handling by checking for missing target server argument.
    
    Condenses multiple conditional statements into a more readable format.
    Returns a boolean indicating the success of the nuke operation.

--- home/crack.js
+++ home/crack.js
@@ -1,37 +1,25 @@
-/** @param {NS} ns */
+/**
+ * @param {NS} ns 
+ * @param {string} targetServer
+ */
 export async function main(ns) {
     let targetServer = ns.args[0]
-
-    if (ns.fileExists("BruteSSH.exe", "home")) {
-        ns.brutessh(targetServer)
-        ns.print(`Executed: BruteSSH.exe`)
+    if (ns.args.length < 1) {
+        throw new Error(`Missing targetServer`)
     }
 
-    if (ns.fileExists("FTPCrack.exe", "home")) {
-        ns.ftpcrack(targetServer)
-        ns.print(`Executed: FTPCrack.exe`)
-    }
-
-    if (ns.fileExists("relaySMTP.exe", "home")) {
-        ns.relaysmtp(targetServer)
-        ns.print(`Executed: relaySMTP.exe`)
-    }
-
-    if (ns.fileExists("HTTPWorm.exe", "home")) {
-        ns.httpworm(targetServer)
-        ns.print(`Executed: HTTPWorm.exe`)
-    }
-
-    if (ns.fileExists("SQLInject.exe", "home")) {
-        ns.sqlinject(targetServer)
-        ns.print(`Executed: SQLInject.exe`)
-    }
-
+    if (ns.fileExists("BruteSSH.exe", "home")) ns.brutessh(targetServer)
+    if (ns.fileExists("FTPCrack.exe", "home")) ns.ftpcrack(targetServer)
+    if (ns.fileExists("relaySMTP.exe", "home")) ns.relaysmtp(targetServer)
+    if (ns.fileExists("HTTPWorm.exe", "home")) ns.httpworm(targetServer)
+    if (ns.fileExists("SQLInject.exe", "home")) ns.sqlinject(targetServer)
 
     try {
         ns.nuke(targetServer)
         ns.tprint(`Nuke complete on ${targetServer}.`)
     } catch (e) {
         ns.tprint(`Nuke failed on ${targetServer}`, e)
+        return false
     }
+    return true
 }

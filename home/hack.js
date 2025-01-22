1aaecf75be32d7d4677bff4d48624d3e888b573f
MPZ-00 <martin.praetzlich@outlook.de>
2025-01-22 03:21:20 +0100

    refactor: 💡logging to use custom print utilities
    Replaces standard print statements with custom logging functions to improve output formatting and readability. Adds error handling for missing target server arguments to enhance robustness.
    
    This change promotes a cleaner output presentation during server operations.

--- home/hack.js
+++ home/hack.js
@@ -1,51 +1,63 @@
+import {
+    cprint,
+    linesplit,
+    linesplitTop
+} from "./utils/customprint.js"
+
 /** @param {NS} ns */
 export async function main(ns) {
     let targetServer = ns.args[0]
+
+    if (ns.args.length < 1) {
+        throw new Error(`Missing targetServer`)
+    }
+
     let securityLevelMin
     let currentSecurityLevel
     let serverMaxMoney
     let serverMoneyAvailable
 
+    linesplitTop(ns)
     while (true) {
         securityLevelMin = ns.getServerMinSecurityLevel(targetServer) // Get the Min Security Level
         currentSecurityLevel = ns.getServerSecurityLevel(targetServer) // Get max money for server
 
-        ns.print("---------------------------------------------------------------")
-        ns.print("Starting attack on " + targetServer + " with " + ns.getHostname() + "...")
+        cprint(ns, "Starting attack on " + targetServer + " with " + ns.getHostname() + "...")
 
         while (currentSecurityLevel > securityLevelMin + 5) {
-            ns.print("---------------------------------------------------------------")
-            ns.print(targetServer + " min security level is " + securityLevelMin)
-            ns.print("Current security level on " + targetServer + " is " + ns.formatNumber(currentSecurityLevel, "0.00") + ".")
-            ns.print("Weakening " + targetServer + " with " + ns.getHostname() + "...")
+            linesplit(ns)
+            cprint(ns, targetServer + " min security level is " + securityLevelMin)
+            cprint(ns, "Current security level on " + targetServer + " is " + ns.formatNumber(currentSecurityLevel, "0.00") + ".")
+            cprint(ns, "Weakening " + targetServer + " with " + ns.getHostname() + "...")
 
             await ns.weaken(targetServer)
             currentSecurityLevel = ns.getServerSecurityLevel(targetServer)
         }
 
-        ns.print("---------------------------------------------------------------")
+        linesplit(ns)
         serverMoneyAvailable = ns.getServerMoneyAvailable(targetServer)
         serverMaxMoney = ns.getServerMaxMoney(targetServer)
 
-        ns.print("Minimum security level on " + targetServer + " reached !!!")
+        cprint(ns, "Minimum security level on " + targetServer + " reached !!!")
 
         while (serverMoneyAvailable < (serverMaxMoney * 0.75)) {
-            ns.print("---------------------------------------------------------------")
-            ns.print(targetServer + " Current Money: " + ns.formatNumber(serverMoneyAvailable, "$0.000a"))
-            ns.print(targetServer + " Max Money: " + ns.formatNumber(serverMaxMoney, "$0.000a"))
-            ns.print("Growing " + targetServer + " with " + ns.getHostname() + " to " + ns.formatNumber(serverMaxMoney * 0.75, "$0.000a") + "...")
+            linesplit(ns)
+            cprint(ns, targetServer + " Current Money: " + ns.formatNumber(serverMoneyAvailable, "$0.000a"))
+            cprint(ns, targetServer + " Max Money: " + ns.formatNumber(serverMaxMoney, "$0.000a"))
+            cprint(ns, "Growing " + targetServer + " with " + ns.getHostname() + " to " + ns.formatNumber(serverMaxMoney * 0.75, "$0.000a") + "...")
 
             await ns.grow(targetServer)
             serverMoneyAvailable = ns.getServerMoneyAvailable(targetServer)
             serverMaxMoney = ns.getServerMaxMoney(targetServer)
         }
 
-        ns.print("---------------------------------------------------------------")
-        ns.print("Optimal current money on " + targetServer + " reached !!!")
-        ns.print(targetServer + " Current Money: " + ns.formatNumber(serverMoneyAvailable, "$0.000a"))
-        ns.print(targetServer + " Max Money: " + ns.formatNumber(serverMaxMoney, "$0.000a"))
-        ns.print("---------------------------------------------------------------")
-        ns.print("Hacking " + targetServer + " with " + ns.getHostname() + "...")
+        linesplit(ns)
+        cprint(ns, "Optimal current money on " + targetServer + " reached !!!")
+        cprint(ns, targetServer + " Current Money: " + ns.formatNumber(serverMoneyAvailable, "$0.000a"))
+        cprint(ns, targetServer + " Max Money: " + ns.formatNumber(serverMaxMoney, "$0.000a"))
+        linesplit(ns)
+        cprint(ns, "Hacking " + targetServer + " with " + ns.getHostname() + "...")
+        linesplit(ns)
 
         await ns.hack(targetServer)
         serverMoneyAvailable = ns.getServerMoneyAvailable(targetServer)

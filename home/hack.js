f67610727607c01246bce2fb789ecdafe471a5d7
MPZ-00 <martin.praetzlich@outlook.de>
2025-02-02 05:50:09 +0100

    refactor: 💡Replace linesplit with linesplitMiddle
    Updates the logging function used during server attacks to provide a more consistent visual output. Changes all instances of `linesplit` to the new `linesplitMiddle` for improved clarity in printed messages during security and money operations.

--- home/hack.js
+++ home/hack.js
@@ -18,11 +18,11 @@ export async function main(ns) {
         securityLevelMin = ns.getServerMinSecurityLevel(targetServer) // Get the Min Security Level
         currentSecurityLevel = ns.getServerSecurityLevel(targetServer) // Get max money for server
 
-        linesplit(ns)
+        linesplitMiddle(ns)
         cprint(ns, "Starting attack on " + targetServer + " with " + ns.getHostname() + "...")
 
         while (currentSecurityLevel > securityLevelMin + 5) {
-            linesplit(ns)
+            linesplitMiddle(ns)
             cprint(ns, targetServer + " min security level is " + securityLevelMin)
             cprint(ns, "Current security level on " + targetServer + " is " + ns.formatNumber(currentSecurityLevel, "0.00") + ".")
             cprint(ns, "Weakening " + targetServer + " with " + ns.getHostname() + "...")
@@ -31,14 +31,14 @@ export async function main(ns) {
             currentSecurityLevel = ns.getServerSecurityLevel(targetServer)
         }
 
-        linesplit(ns)
+        linesplitMiddle(ns)
         serverMoneyAvailable = ns.getServerMoneyAvailable(targetServer)
         serverMaxMoney = ns.getServerMaxMoney(targetServer)
 
         cprint(ns, "Minimum security level on " + targetServer + " reached !!!")
 
         while (serverMoneyAvailable < (serverMaxMoney * 0.75)) {
-            linesplit(ns)
+            linesplitMiddle(ns)
             cprint(ns, "" + targetServer + " Current Money: " + ns.formatNumber(serverMoneyAvailable, 3))
             cprint(ns, "" + targetServer + " Max Money: " + ns.formatNumber(serverMaxMoney, 3))
             cprint(ns, "Growing " + targetServer + " with " + ns.getHostname() + " to " + ns.formatNumber(serverMaxMoney * 0.75, 3) + "...")
@@ -48,20 +48,18 @@ export async function main(ns) {
             serverMaxMoney = ns.getServerMaxMoney(targetServer)
         }
 
-        linesplit(ns)
+        linesplitMiddle(ns)
         cprint(ns, "Optimal current money on " + targetServer + " reached !!!")
         cprint(ns, targetServer + " Current Money: " + ns.formatNumber(serverMoneyAvailable, 3))
         cprint(ns, targetServer + " Max Money: " + ns.formatNumber(serverMaxMoney, 3))
-        linesplit(ns)
+        linesplitMiddle(ns)
         cprint(ns, "Hacking " + targetServer + " with " + ns.getHostname() + "...")
 
         await ns.hack(targetServer)
-        serverMoneyAvailable = ns.getServerMoneyAvailable(targetServer)
-        serverMaxMoney = ns.getServerMaxMoney(targetServer)
     }
 }
-function linesplit(t, n = !1, i = 50, e = "─", l = "├", p = "┤") { let r = Math.max(i, 10); r -= l.length + p.length; let o = e.repeat(r), f = l + o + p; n ? t.tprint(f) : t.print(f) }
-function linesplitTop(t, n = !1, i = 50, e = "─", l = "┌", p = "┐") { linesplit(t, n, i, e, l, p) }
-function linesplitBottom(t, n = !1, i = 50, e = "─", l = "└", p = "┘") { linesplit(t, n, i, e, l, p) }
+function linesplitMiddle(t, n = !1, i = 50, e = "─", l = "├", p = "┤") { let r = Math.max(i, 10); r -= l.length + p.length; let o = e.repeat(r), f = l + o + p; n ? t.tprint(f) : t.print(f) }
+function linesplitTop(t, n = !1, i = 50, e = "─", l = "┌", p = "┐") { linesplitMiddle(t, n, i, e, l, p) }
+function linesplitBottom(t, n = !1, i = 50, e = "─", l = "└", p = "┘") { linesplitMiddle(t, n, i, e, l, p) }
 function cprint(t, n, i = !1, e = 50, l = "│", p = "│") { let r = Math.max(e, 50); r -= l.length + p.length; let o = ""; for (let f of n) { if (o.length + 1 > r) { let $ = o.padEnd(r, " "), c = l + $ + p; i ? t.tprint(c) : t.print(c), o = "" } o += f } if (o.length > 0) { let x = o.padEnd(r, " "), g = l + x + p; i ? t.tprint(g) : t.print(g) } }
 function tcprint(t, n, i = 50) { cprint(t, n, !0, i) }

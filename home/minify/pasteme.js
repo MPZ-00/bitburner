7f838425dc365e73a774195d1a3936af298abe12
MPZ-00 <martin.praetzlich@outlook.de>
2025-02-02 05:50:19 +0100

    feat: 🎸Add reusable line-splitting functions
    Introduces a new constant for maximum line length to enhance readability.
    
    Adds a `linesplitMiddle` function to centralize line-splitting logic, improving code maintainability.
    
    Refactors existing line-splitting functions to utilize the new function.

--- home/minify/pasteme.js
+++ home/minify/pasteme.js
@@ -1,5 +1,6 @@
-function linesplit(t, n = !1, i = 50, e = "─", l = "├", p = "┤") { let r = Math.max(i, 10); r -= l.length + p.length; let o = e.repeat(r), f = l + o + p; n ? t.tprint(f) : t.print(f) }
-function linesplitTop(t, n = !1, i = 50, e = "─", l = "┌", p = "┐") { linesplit(t, n, i, e, l, p) }
-function linesplitBottom(t, n = !1, i = 50, e = "─", l = "└", p = "┘") { linesplit(t, n, i, e, l, p) }
-function cprint(t, n, i = !1, e = 50, l = "│", p = "│") { let r = Math.max(e, 50); r -= l.length + p.length; let o = ""; for (let f of n) { if (o.length + 1 > r) { let $ = o.padEnd(r, " "), c = l + $ + p; i ? t.tprint(c) : t.print(c), o = "" } o += f } if (o.length > 0) { let x = o.padEnd(r, " "), g = l + x + p; i ? t.tprint(g) : t.print(g) } }
-function tcprint(t, n, i = 50) { cprint(t, n, !0, i) }
+const maxLineLength = 50
+function linesplitMiddle(t, n = false, i = maxLineLength, e = "─", l = "├", p = "┤") { let r = Math.max(i, maxLineLength); r -= l.length + p.length; let o = e.repeat(r), f = l + o + p; n ? t.tprint(f) : t.print(f) }
+function linesplitTop(t, n = false, i = maxLineLength, e = "─", l = "┌", p = "┐") { linesplitMiddle(t, n, i, e, l, p) }
+function linesplitBottom(t, n = false, i = maxLineLength, e = "─", l = "└", p = "┘") { linesplitMiddle(t, n, i, e, l, p) }
+function cprint(t, n, i = false, e = maxLineLength, l = "│", p = "│") { let r = Math.max(e, maxLineLength); r -= l.length + p.length; let o = ""; for (let f of n) { if (o.length + 1 > r) { let $ = o.padEnd(r, " "), c = l + $ + p; i ? t.tprint(c) : t.print(c), o = "" } o += f } if (o.length > 0) { let x = o.padEnd(r, " "), g = l + x + p; i ? t.tprint(g) : t.print(g) } }
+function tcprint(t, n, i = maxLineLength) { cprint(t, n, true, i) }
